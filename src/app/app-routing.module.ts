8e485fb381b8caca6c6642f2db85d9e206fc8391
obed-hurtado12 <20193tn117@utez.edu.mx>
2023-09-19 19:30:31 -0600

    [ADD] feature/Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './modules/news/news.component';
+import { AboutComponent } from './modules/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"news", component: NewsComponent},
+  {path:"about", component: AboutComponent},
+  {path:"**", redirectTo: "/", pathMatch:"full"}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], 
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './modules/news/news.component';
+import { AboutComponent } from './modules/about/about.component';
+import { ChatComponent } from './modules/chat/chat.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    AboutComponent,
+    ChatComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
